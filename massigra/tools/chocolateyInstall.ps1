d849adbf2eb8c5be0861b8f9f801cfac6fe3aea7
Pine Mizune <pinemz@gmail.com>
2014-12-12 08:33:36 +0900

    Add MassiGra

---
+++ massigra/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = '{{PackageName}}'
+$packageTitle = 'MassiGra'
+$version = '{{PackageVersion}}'
+$url = '{{DownloadUrl}}'
+
+try {
+	$binRoot = Get-BinRoot
+	Write-Debug "Bin Root is $binRoot"
+
+	$installDir = Join-Path $binRoot $packageTitle
+	Write-Debug "Install Dir is $installDir"
+
+	Install-ChocolateyZipPackage $packageName $url $installDir
+	Write-ChocolateySuccess $packagename
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
