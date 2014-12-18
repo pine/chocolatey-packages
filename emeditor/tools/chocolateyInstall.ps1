1a669064e331cdc15c8c47e614683c46636b84b3
Pine Mizune <pinemz@gmail.com>
2014-12-18 12:32:42 +0900

    Add EmEditor (x86/x64)

---
+++ emeditor/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = '{{PackageName}}'
+$version = '{{PackageVersion}}'
+$fileType = 'exe'
+$installArgs = '/exenoui /q'
+$url = 'https://www.emurasoft.com/pub/emed32_{{PackageVersion}}.exe'
+$url64 = 'https://www.emurasoft.com/pub/emed64_{{PackageVersion}}.exe'
+
+try {
+  $osBitness = Get-ProcessorBits
+  $is64bit = $osBitness -eq 64
+  
+  if ($is64bit) {
+    Install-ChocolateyPackage $packageName $fileType $installArgs $url64
+  }
+  
+  else {
+    Install-ChocolateyPackage $packageName $fileType $installArgs $url
+  }
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
