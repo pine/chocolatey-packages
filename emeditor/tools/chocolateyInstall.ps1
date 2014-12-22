ac8845f8cd589e9c160a5a58a63fed7d2762ea95
Pine Mizune <pinemz@gmail.com>
2014-12-22 16:10:08 +0900

    Fix EmEditor

--- emeditor/tools/chocolateyInstall.ps1
+++ emeditor/tools/chocolateyInstall.ps1
@@ -5,18 +5,4 @@ $installArgs = '/exenoui /q'
 $url = 'https://www.emurasoft.com/pub/emed32_{{PackageVersion}}.exe'
 $url64 = 'https://www.emurasoft.com/pub/emed64_{{PackageVersion}}.exe'
 
-try {
-  $osBitness = Get-ProcessorBits
-  $is64bit = $osBitness -eq 64
-  
-  if ($is64bit) {
-    Install-ChocolateyPackage $packageName $fileType $installArgs $url64
-  }
-  
-  else {
-    Install-ChocolateyPackage $packageName $fileType $installArgs $url
-  }
-} catch {
-  Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw
-}
+Install-ChocolateyPackage $packageName $fileType $installArgs $url $url64
