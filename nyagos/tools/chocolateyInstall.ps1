198096fd8f2e231368ec4568de92b88ab6cc73c1
Pine Mizune <pinemz@gmail.com>
2014-12-23 23:46:13 +0900

    Add NYAGOS

---
+++ nyagos/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'nyagos'
+$packageTitle = 'NYAGOS'
+$version = '4.0.3.0'
+$url = 'https://github.com/zetamatta/nyagos/releases/download/4.0.3_0/nyagos-4.0.3_0.zip'
+
+try {
+	$binRoot = Get-BinRoot
+	Write-Debug "Bin Root is $binRoot"
+
+	$installDir = Join-Path $binRoot $packageTitle
+	Write-Debug "Install Dir is $installDir"
+
+	Install-ChocolateyZipPackage $packageName $url $installDir
+	Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
