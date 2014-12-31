e5e2de3cbcf37207af376090be67be318d57427f
MIZUNE Pine <pinemz@gmail.com>
2015-01-01 06:04:16 +0900

    Add vb5runtime

---
+++ vb5runtime/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'vb5runtime'
+$fileName = 'Msvbvm50'
+$url = 'http://download.microsoft.com/download/vb50pro/utility/1/win98/EN-US/Msvbvm50.exe'
+
+try {
+  $filePath = "$env:TEMP\chocolatey\$packageName"
+  $fileFullPath = "$filePath\${fileName}.exe"
+  
+  $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+  $ahkFile = "$scriptPath\vb5runtime.ahk"
+  
+  NewItem $filePath -directory -force
+  Get-ChocolateyWebFile $packageName $fileFullPath $url
+  
+  Start-Process 'AutoHotkey' $ahkFile
+  Start-Process $fileFullPath
+  
+  Wait-Process -Name $fileName
+  
+  Write-ChocolateySuccess $packageName
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
---
+++ vb5runtime/tools/vb5runtime.ahk
@@ -0,0 +1,10 @@
+﻿SetTitleMatchMode, 2
+
+WinWait, VB Runtime Installation ahk_class #32770
+
+ControlClick, Button1, VB Runtime Installation ahk_class #32770
+
+WinWait, VB Runtime Installation ahk_class #32770, successfully
+ControlClick, Button1, VB Runtime Installation ahk_class #32770
+
+WinWaitClose, VB Runtime Installation ahk_class #32770, successfully
