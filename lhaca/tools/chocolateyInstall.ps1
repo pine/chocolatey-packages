88d50f3fcd9cc0f92d581ec29bdc3125f03b13ea
MIZUNE Pine <pinemz@gmail.com>
2015-01-01 07:37:25 +0900

    Add +Lhaca

---
+++ lhaca/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿$packageName = 'lhaca'
+$fileName = 'Lhaca076'
+$url = 'http://park8.wakwak.com/~app/Lhaca/Lhaca076.EXE'
+
+try {
+  $filePath = "$env:TEMP\chocolatey\$packageName"
+  $fileFullPath = "$filePath\${fileName}.exe"
+  
+  $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+  $ahkFile = "$scriptPath\${packageName}.ahk"
+  
+  New-Item $filePath -Directory -Force
+  Get-ChocolateyWebFile $packageName $fileFullPath $url
+  
+  Start-Process 'AutoHotkey' $ahkFile
+  Start-Process $fileFullPath
+  
+  Wait-Process -Name $fileName
+  Remove-Item $filePath -Force -Recurse
+  
+  Write-ChocolateySuccess $packageName
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
---
+++ lhaca/tools/lhaca.ahk
@@ -0,0 +1,5 @@
+﻿SetTitleMatchMode, 2
+
+WinWait, +Lhaca ahk_class #32770, , 30
+ControlClick, Button2, +Lhaca ahk_class #32770
+WinWaitClose, +Lhaca ahk_class #32770, , 60
