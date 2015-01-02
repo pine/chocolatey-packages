1abac58adc8d6744325dcbcc44a02a8a4dace59f
MIZUNE Pine <pinemz@gmail.com>
2015-01-02 10:59:31 +0900

    Fixed #5

--- terapad/tools/chocolateyInstall.ps1
+++ terapad/tools/chocolateyInstall.ps1
@@ -10,6 +10,7 @@ try {
   $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
   $ahkFile = "$scriptPath\terapad.ahk"
   
+  New-Item $filePath -Force -Directory
   Get-ChocolateyWebFile $packageName $fileFullPath $url
   
   Start-Process 'AutoHotkey' $ahkFile
