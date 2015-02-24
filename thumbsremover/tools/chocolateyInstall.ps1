e6f860edb3415bf57aabfc690c630a9a096cdb9b
Pine Mizune <pinemz@gmail.com>
2015-02-24 12:29:30 +0900

    Add Thumbs Remover

---
+++ thumbsremover/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'thumbsremover'
+$version = '1.6.1.280'
+$fileType = 'exe'
+$installArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.xtreme-lab.net/downloads/tr161_setup.exe'
+
+Install-ChocolateyPackage $packageName $fileType $installArgs $url
