f5cd70b44b52d91a35aa2ea812569f6223ab2345
Pine Mizune <pinemz@gmail.com>
2015-03-04 02:06:32 +0900

    Fix package name

--- lha/tools/chocolateyInstall.ps1
+++ lha/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName = 'lha.portable'
+﻿$packageName = 'lha'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $url = 'http://sourceforge.net/projects/gnuwin32/files/lha/1.14i-1/lha-1.14i-1-bin.zip/download'
 
