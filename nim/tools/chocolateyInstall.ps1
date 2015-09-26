1e467feb7d2096aba4c69611c1f76df443635032
Pine Mizune <pinemz@gmail.com>
2015-09-26 20:12:25 +0900

    Update Nim version

--- nim/tools/chocolateyInstall.ps1
+++ nim/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'nim'
-$version = '0.10.2'
+$version = '0.11.2'
 $fileType = 'exe'
 $installArgs = '/S'
-$url = 'http://nim-lang.org/download/nim-0.10.2_x32.exe'
-$url64 = 'http://nim-lang.org/download/nim-0.10.2_x64.exe'
+$url = 'http://nim-lang.org/download/nim-0.11.2_x32.exe'
+$url64 = 'http://nim-lang.org/download/nim-0.11.2_x64.exe'
 
 Install-ChocolateyPackage $packageName $fileType $installArgs $url $url64
