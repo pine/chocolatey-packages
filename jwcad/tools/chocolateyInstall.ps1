567154d414dec1aee354a73adf861c8adda783a4
Pine Mizune <pinemz@gmail.com>
2017-07-12 00:45:57 +0900

    feat(jwcad): good bye Ketarin

--- jwcad/tools/chocolateyInstall.ps1
+++ jwcad/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = '{{PackageName}}'
-$version = '{{PackageVersion}}'
+﻿$packageName = 'jwcad'
+$version = '8.03.1'
 $fileType = 'exe'
 $installArgs = '/s /v" /qn"'
-$url = '{{DownloadUrl}}'
+$url = 'http://www.jwcad.net/download/jww803a.exe'
 
 Install-ChocolateyPackage $packageName $fileType $installArgs $url
