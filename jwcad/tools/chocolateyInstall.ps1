059dc6d134d4a7d79f82c7cfd0dbddfa0ae13e0a
Pine Mizune <pinemz@gmail.com>
2017-07-12 01:27:55 +0900

    fix(jwcad): support checksum

--- jwcad/tools/chocolateyInstall.ps1
+++ jwcad/tools/chocolateyInstall.ps1
@@ -3,5 +3,8 @@ $version = '8.03.1'
 $fileType = 'exe'
 $installArgs = '/s /v" /qn"'
 $url = 'http://www.jwcad.net/download/jww803a.exe'
+$checksum = 'AFA9B00388DD52D4198E30C2D94718D4896E87F0236DFED070ACF74AA983A332'
+$checksumType = 'sha256'
 
-Install-ChocolateyPackage $packageName $fileType $installArgs $url
+Install-ChocolateyPackage $packageName $fileType $installArgs $url `
+    -Checksum $checksum -ChecksumType $checksumType
